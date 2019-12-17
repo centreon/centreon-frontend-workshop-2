a8a15f20c768ab58df1e0b183ec882ec7da669ab
Bruno d'Auria <bdauria@centreon.com>
2019-12-17 11:59:40 +0100

    Implement Login spec

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,7 +1,67 @@
 /// <reference types="Cypress" />
 
-context('Login', () => {
-	beforeEach(() => {});
+import connection from '../support/db';
 
-	it('does something', () => {});
+const username = 'Bobby';
+
+context('Login', () => {
+	beforeEach(() => {
+		cy.visit('/login');
+	});
+
+	it('displays form errors if any and prevents from submitting', () => {
+		cy.clickOnLoginInput();
+		cy.clickOnPasswordInput();
+		cy.contains('Login is required');
+
+		cy.get('button').should('be.disabled');
+
+		cy.clickOnLoginInput();
+		cy.contains('Password is required');
+		cy.get('button').should('be.disabled');
+	});
+
+	it('displays an error if the login and password have no match', () => {
+		cy.typeOnLoginInput('Bobbby');
+
+		cy.typeOnPasswordInput('goodpassword');
+
+		cy.get('button').click();
+
+		cy.contains('Login or password invalid');
+	});
+
+	it('redirects to homepage and welcomes the user when login is successful', () => {
+		cy.fixture('Bobby.json').then(connection().put);
+
+		cy.typeOnLoginInput(username);
+
+		cy.typeOnPasswordInput('goodpassword');
+
+		cy.get('button').click();
+
+		cy.contains(`Welcome ${username}`);
+
+		cy.url().should('eq', Cypress.config().baseUrl + '/');
+	});
+
+	it('logs out the User when the logout button is clicked', () => {
+		cy.fixture('Bobby.json').then(connection().put);
+
+		cy.typeOnLoginInput(username);
+
+		cy.typeOnPasswordInput('goodpassword');
+
+		cy.get('button').click();
+
+		cy.contains('Logout').click();
+
+		cy.contains('Logout').should('not.exist');
+
+		cy.contains('Please register or login to continue');
+
+		cy.contains('Login');
+
+		cy.contains('Register');
+	});
 });
